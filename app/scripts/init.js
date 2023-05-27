d0c7f3810c3e96c5ddab209cbe50b7d5849082dd
Pavel <pavelfedorov929@gmail.com>
2023-05-27 16:41:22 +0300

    done

---
+++ app/scripts/init.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    /* const animatedItems = document.querySelectorAll('.animated');
+
+    const callback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+                observer.unobserve(entry.target)
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }
+
+    const options = {
+        // root: по умолчанию window, но можно задать любой элемент-контейнер
+        rootMargin: '0px 0px 75px 0px',
+        threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    animatedItems.forEach((item) => observer.observe(item)); */
+});
